3967d552ac19b89ff9c4aeec0c0d07d4b8aa35ac
AndyLee <andylee9202@gmail.com>
2026-01-03 12:26:09 +0700

    feat: Add submit KYC API, signing, tests, and improve client logging

---
+++ client_test.go
@@ -0,0 +1,46 @@
+package goaliniex_test
+
+import (
+	"log/slog"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/TranVuGiang/goaliniex"
+)
+
+func newTestClient(t *testing.T) *goaliniex.Client {
+	t.Helper()
+
+	privateKey, err := os.ReadFile("./alix-private-key.pem")
+	if err != nil {
+		t.Skipf("skipping test: unable to read private key: %v", err)
+	}
+
+	partnerCode := strings.TrimSpace(os.Getenv("ALIX_PARTNER_CODE"))
+	if partnerCode == "" {
+		t.Skip("skipping test: ALIX_PARTNER_CODE not set")
+	}
+
+	secretKey := strings.TrimSpace(os.Getenv("ALIX_SECRET_KEY"))
+	if secretKey == "" {
+		t.Skip("skipping test: ALIX_SECRET_KEY not set")
+	}
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource:   true,
+		Level:       slog.LevelDebug,
+		ReplaceAttr: nil,
+	}))
+
+	client := goaliniex.NewClient(
+		"https://sandbox.alixpay.com",
+		partnerCode,
+		secretKey,
+		privateKey,
+		goaliniex.WithDebug(true),
+		goaliniex.WithLogger(logger),
+	)
+
+	return client
+}
--- go.mod
+++ go.mod
@@ -1,10 +1,3 @@
 module github.com/TranVuGiang/goaliniex
 
-go 1.25.4
-
-require resty.dev/v3 v3.0.0-beta.5
-
-require (
-	github.com/andyle182810/gframework v0.0.3 // indirect
-	golang.org/x/net v0.43.0 // indirect
-)
+go 1.22.0
